f54a7f33d87a280e73a2f38fc8681de7a26b63ab
Navera <naverazafar23@gmail.com>
2021-12-09 18:01:43 -0800

    clicker

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ const themeSwitch = document.querySelector('div.toggle');
 themeSwitch.addEventListener('change', () => {
   document.body.classList.toggle('darkmode');
 });
+
+function createImage() {
+  return `
+  <div><img class="image" src="images/emoji.png" alt="Emoji"></img><div>
+  `
+}
+
+function renderImage() {
+  const div = document.querySelector('div.main')
+
+  div.insertAdjacentHTML('beforeend', createImage())
+}
+
+const button = document.querySelector('button.clickerbutton');
+
+function clckLog (evt) {
+  renderImage()
+}
+
+button.addEventListener('click', clckLog)
